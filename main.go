53a603100468838a1db21de777045fa2fdd48b03
Elias Jurado <ridimdev@gmail.com>
2024-03-20 08:26:43 -0500

    funciones recursivas - ejemplo factorial

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"advance-functions/recursiva"
 	"advance-functions/variadica"
 	"fmt"
 	"log"
@@ -8,8 +9,10 @@ import (
 
 func main() {
 	log.SetFlags(0)
+
 	fmt.Println(variadica.Suma("Tomas", 12, 4, 78, 56))
 	fmt.Println(variadica.Suma("Cristina", 10, 20, 30, 40, 50))
-	variadica.ImprimirDatos("Hola",35,"Elias","Carla",45.45,"Tomas",-1)
-}
+	variadica.ImprimirDatos("Hola", 35, "Elias", "Carla", 45.45, "Tomas", -1)
 
+	fmt.Println(recursiva.Factorial(5))
+}
